3417e67438e32942305cb0f62eba7cb14d926605
Ravneet193 <taneja.ravneet@gmail.com>
2018-04-08 13:05:37 +0530

    committing New Changes

---
+++ app/src/main/java/com/example/dell/porterapplication/TripHistory.java
@@ -0,0 +1,13 @@
+package com.example.dell.porterapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_history);
+    }
+}
